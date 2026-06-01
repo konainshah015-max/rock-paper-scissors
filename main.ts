66d84b6b2e8d4f99003dc340159f2281de547da3
konainshah015-max <konain.shah015@gmail.com>
2026-05-31 21:49:19 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+let hand = 0
+input.onGesture(Gesture.Shake, function () {
+    hand = randint(1, 3)
+    if (hand == 1) {
+        basic.showIcon(IconNames.SmallSquare)
+    } else if (hand == 2) {
+        basic.showIcon(IconNames.Square)
+    } else if (hand == 3) {
+        basic.showIcon(IconNames.Meh)
+    } else {
+    	
+    }
+})
